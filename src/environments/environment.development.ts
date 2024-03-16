45cfab1fdbe29addffce9856e028964bffd30cc8
Gerardo Rodriguez <garc@outlook.com>
2024-03-16 11:44:46 -0600

    Guest registration modal added to sorteos/detalles and lottery card

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // url: "https://sorteos-devtalles-06ee9219895b.herokuapp.com/api/v1",
-  url: "http://localhost:3008/api/v1/",
+  url: "https://sorteos-devtalles-06ee9219895b.herokuapp.com/api/v1",
 };
