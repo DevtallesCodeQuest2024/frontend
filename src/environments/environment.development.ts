38ca123df40619f8461c8b1241c89b6aa0bad0e6
Hiram Chavez <virtual.liga@gmail.com>
2024-03-17 14:37:44 -0600

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   url: "http://localhost:4006/api/v1",
+  DISCORD_CLIENT_ID:'1218992799291277313',
+  DISCORD_CALLBACK_URL:'http://localhost:4006/api/v1/auth/discord/callback'
 };
