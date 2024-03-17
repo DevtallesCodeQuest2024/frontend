80a23b4335567fb3e6ebecff5597b141088325a4
Hiram Chavez <virtual.liga@gmail.com>
2024-03-17 14:47:08 -0600

    Update auth-api.service.ts

--- src/app/core/api/auth-api.service.ts
+++ src/app/core/api/auth-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from 'environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { IUser } from '@app/core/models/user';
 import { ILogin } from '@app/core/models/auth';
 import { IResponse } from "../models/apiResponse";
