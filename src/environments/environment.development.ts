3878723c9edfcf77c4e34ead38fb8a6e29a8adb0
Hiram Chavez <virtual.liga@gmail.com>
2024-03-17 23:44:30 -0600

    Adding button Choose winner

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  url: "http://localhost:4006/api/v1",
+  url: "http://localhost:3800/api/v1",
   DISCORD_CLIENT_ID:'1219062115202240602',
   DISCORD_CALLBACK_URL:'http://localhost:3800/api/v1/auth/discord/callback'
 };
